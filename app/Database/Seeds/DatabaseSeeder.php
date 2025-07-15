f9884769ad6ac79a5d12b696f78226b72bf2f330
sandysan1234 <rahmahdiandian@gmail.com>
2025-07-15 20:08:08 +0700

    lifetime and location create

---
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('CostcenterSeeder');
+        $this->call('LifetimeSeeder');
+        $this->call('PemasokSeeder');
+        $this->call('PlantSeeder');
+    }
+}
