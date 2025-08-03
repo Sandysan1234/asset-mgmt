2e310c1a92faa0b8872e82ce3387d5a383660790
sandysan1234 <rahmahdiandian@gmail.com>
2025-08-03 22:37:20 +0700

    make asset detail UI

--- app/Controllers/Qrbarcode.php
+++ app/Controllers/Qrbarcode.php
@@ -14,59 +14,59 @@ use Endroid\QrCode\Writer\PngWriter;
 
 class Qrbarcode extends BaseController
 {
-  protected $assetModel;
-  public function __construct()
-  {
-    $this->assetModel = new AssetModel();
-  }
-  public function index()
-  {
-    $data = [
-      'title' => 'Generate QR Code | Asset Managed',
-      'qrList' => []
-    ];
-    return view('qrcode/index', $data);
-  }
-  public function multiple()
-  {
-    $jumlah = $this->request->getPost('jumlah');
-
-    // $assets = $this->assetModel->findAll($jumlah);
-    $assets = $this->assetModel->getWithRelasi(null, $jumlah); // ambil 10 data
-
-    $writer = new PngWriter();
-    $qrList = [];
-    // dd($assets);
-    foreach ($assets as $as) {
-      $qrcode = new QrCode(
-        data: base_url('asset/detail/' . $as['id_asset']),
-        encoding: new Encoding('UTF-8'),
-        errorCorrectionLevel: ErrorCorrectionLevel::Low,
-        size: 300,
-        margin: 10,
-        roundBlockSizeMode: RoundBlockSizeMode::Margin,
-        foregroundColor: new Color(0, 0, 0),
-        backgroundColor: new Color(255, 255, 255)
-      );
-
-      $logoPath = FCPATH . 'assets/images/logo-jmi.png';
-      $logo = file_exists($logoPath) ? new Logo(
-        path: $logoPath,
-        resizeToWidth: 50,
-        punchoutBackground: true
-      ) : null;
-
-      $result = $writer->write($qrcode, $logo, null);
-      $qrList[] = [
-        'nama_asset'    => $as['nama_asset'],
-        'no_asset'      => $as['no_asset'],
-        'tgl_perolehan' => $as['tgl_perolehan'],
-        'qr' => $result->getDataUri()
-      ];
+    protected $assetModel;
+    public function __construct()
+    {
+        $this->assetModel = new AssetModel();
     }
-    return view('qrcode/index', [
-      'title' => 'QR Code Asset',
-      'qrList' => $qrList
-    ]);
-  }
+    public function index()
+    {
+        $data = [
+            'title' => 'Generate QR Code | Asset Managed',
+            'qrList' => []
+        ];
+        return view('qrcode/index', $data);
+    }
+    public function multiple()
+    {
+        $jumlah = $this->request->getPost('jumlah');
+
+        // $assets = $this->assetModel->findAll($jumlah);
+        $assets = $this->assetModel->getWithRelasi(null, $jumlah); // ambil 10 data
+
+        $writer = new PngWriter();
+        $qrList = [];
+        // dd($assets);
+        foreach ($assets as $as) {
+            $qrcode = new QrCode(
+                data: base_url('asset/detail/' . $as['id_asset']),
+                encoding: new Encoding('UTF-8'),
+                errorCorrectionLevel: ErrorCorrectionLevel::Low,
+                size: 300,
+                margin: 10,
+                roundBlockSizeMode: RoundBlockSizeMode::Margin,
+                foregroundColor: new Color(0, 0, 0),
+                backgroundColor: new Color(255, 255, 255)
+            );
+
+            $logoPath = FCPATH . 'assets/images/logo-jmi.png';
+            $logo = file_exists($logoPath) ? new Logo(
+                path: $logoPath,
+                resizeToWidth: 50,
+                punchoutBackground: true
+            ) : null;
+
+            $result = $writer->write($qrcode, $logo, null);
+            $qrList[] = [
+                'nama_asset'    => $as['nama_asset'],
+                'no_asset'      => $as['no_asset'],
+                'tgl_perolehan' => $as['tgl_perolehan'],
+                'qr' => $result->getDataUri()
+            ];
+        }
+        return view('qrcode/index', [
+            'title' => 'QR Code Asset',
+            'qrList' => $qrList
+        ]);
+    }
 }
--- app/Views/asset/detail.php
+++ app/Views/asset/detail.php
@@ -6,13 +6,20 @@
     <div class="col-md-4 col-xxl-3">
       <div class="card">
         <div class="card-body">
-          <div class="d-flex align-items-center justify-content-between mb-3">
-            <h5 class="mb-0"><?= $asset['nama_asset']; ?></h5>
-          </div>
           <div class="card">
             <img class="" src="<?= $qr; ?>" alt="">
           </div>
+          <div class="text-center">
+            <h4 class=""><?= $asset['nama_asset']; ?></h4>
+            <p><?= $asset['no_asset']; ?></p>
+          </div>
 
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class=""><i class="ti ti-notes"></i> Spesifikasi Asset</h5>
+          <p><?= $asset['spek']; ?></p>
         </div>
       </div>
       <div class="card">
@@ -122,150 +129,163 @@
       </div>
     </div>
     <div class="col-md-8 col-xxl-9">
-      <div class="row">
-        <div class="col-md-6 col-xxl-4">
-          <div class="card">
-            <div class="card-body">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item px-0">
-                  <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 ms-3 mt-2">
-                      <div class="row g-1">
-                        <div class="col-6">
-                          <h6 class="mt-1">No. Asset</h6>
-                        </div>
-                        <div class="col-6 text-end">
-                          <p class="text-success"><?= $asset['no_asset']; ?></p>
-                        </div>
-                      </div>
+      <div class="card">
+        <div class="card-body">
+          <div class="d-flex align-items-center justify-content-between mb-3">
+            <h5 class="text-primary mb-0"><i class="ti ti-list-search"></i> Identifikasi Lainnya</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item px-0">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">Serial Number</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p class=""><?= $asset['serial_number']; ?></p>
                     </div>
                   </div>
-                </li>
-                <li class="list-group-item px-0">
-                  <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 ms-3 mt-2">
-                      <div class="row g-1">
-                        <div class="col-6">
-                          <h6 class="mt-1">Sub Asset</h6>
-                        </div>
-                        <div class="col-6 text-end">
-                          <p class="text-warning"><?= $asset['sub_asset']; ?></p>
-                        </div>
-                      </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">Batch Number</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p><?= $asset['batch_number']; ?></p>
                     </div>
                   </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-xxl-4">
-          <div class="card">
-            <div class="card-body"></div>
-          </div>
-        </div>
-        <div class="col-md-6 col-xxl-4">
-          <div class="card">
-            <div class="card-body"></div>
-          </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0 border-bottom">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">No. Purchase Order</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p><?= $asset['no_po']; ?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+          </ul>
         </div>
       </div>
-    </div>
-    <div class="col-md-12 col-xxl-12">
       <div class="card">
         <div class="card-body">
-          <h5>Details</h5>
-          <p><?= $asset['spek']; ?></p>
-          <p>Tanggal Perolehan : <?= (new DateTime($asset['tgl_perolehan']))->format('d-m-Y');?></p>
-          <p>No. Purchase Order: <?= $asset['no_po'];?></p>
-          <p>Serial Number: <?= $asset['serial_number'];?></p>
-          <p>Batch Number: <?= $asset['batch_number'];?></p>
-          <p>Merek: <?= $asset['merek'];?></p>
-          <p>Status: <?= $asset['status'];?></p>
-          <p>PIC : <?= $asset['user_fullname'];?> - <?= $asset['user_username'];?></p>
-          <p>User Asset:  <?= $asset['pic_fullname'];?> - <?= $asset['pic_username'];?></p>
-        </div>
-
-      </div>
-    </div>
-
-
-
-
-
-    <div class="col-md-3 bg-warning p-3">
-      <div class="print-area">
-        <h5 class="mb-3">Income Overview</h5>
-        <div class="card my-4">
-          <img src="<?= $qr ?>" alt="">
-          <!-- <img src="<?= base_url(); ?>assets/images/logo-jmi.png" class="card-img-top" alt="QR Code"> -->
-          <div class="card-body">
-            <p class="card-text text-center"><?= $asset['no_asset']; ?></p>
+          <div class="d-flex align-items-center justify-content-between mb-3">
+            <h5 class="mb-0"><i class="ti ti-file-check"></i> Administrasi & Status</h5>
           </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item px-0">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">Tanggal Perolehan</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p class=""><?= $asset['tgl_perolehan']; ?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">Harga</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p>Rp. <?= $asset['harga']; ?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class=" mt-1">Lifetime</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p><?= $asset['masa_berlaku']; ?> Tahun</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0 border-bottom">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">Status</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <?php
+                      $statusList = [
+                        0 => ['label' => 'Proses Pelepasan',     'class' => 'bg-primary'],
+                        1 => ['label' => 'Proses Penghentian',   'class' => 'bg-warning'],
+                        2 => ['label' => 'Penggabungan',         'class' => 'bg-secondary'],
+                        3 => ['label' => 'Proses Mutasi ',       'class' => 'bg-info'],
+                        4 => ['label' => 'Non-Aktif ',           'class' => 'bg-danger'],
+                        5 => ['label' => 'Aktif',                'class' => 'bg-success'],
+                      ];
+                      $currentStatus = $statusList[$asset['status']] ?? ['label' => 'Unknown', 'class' => 'bg-dark'];
+                      ?>
+
+                      <p><span class="badge <?= $currentStatus['class']; ?> rounded-2"><?= $currentStatus['label']; ?></span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0 border-bottom">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">PIC</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p><?= $asset['pic_username']; ?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item px-0 border-bottom">
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1 ms-3">
+                  <div class="row g-1">
+                    <div class="col-6">
+                      <h6 class="mt-1">User Asset</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <p><?= $asset['user_username']; ?></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+
+          </ul>
         </div>
       </div>
     </div>
-    <div class="col-md-8  bg-info">
-      2 of 2
-    </div>
-    <div class="col-md-12 col-xl-8 ">
-      <h5 class="mb-3">Sales Report</h5>
-      <div class="card">
-        <div class="card-body">
-          <h6 class="mb-2 f-w-400 text-muted">This Week Statistics</h6>
-          <h3 class="mb-0">$7,650</h3>
-          <div id="sales-report-chart"></div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="col-md-12 col-xl-8">
-      <h5 class="mb-3">Recent Orders</h5>
-      <div class="card tbl-card">
-        <div class="card-body">
-          <div class="table-responsive">
-            kdsfasdfj
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="col-2 bg-secondary">
-      <div class="card my-4" style="width: 10rem;">
-        <!-- <img src="?= $img ?>" alt=""> -->
-        <img src="<?= base_url(); ?>assets/images/logo-jmi.png" class="card-img-top" alt="QR Code">
-        <div class="card-body">
-          <p class="card-text text-center"><?= $asset['serial_number']; ?></p>
-        </div>
-      </div>
-
-    </div>
-    <div class="card mb-3 bg-warning">
-      <div class="row g-0 bg-info">
-        <div class="col-md-4 p-4 bg-secondary">
-          <img src="<?= base_url(); ?>assets/images/logo-jmi.svg" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-3 my-4 bg-primary">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Laptop taraktung jesss</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 <?= $this->endSection(); ?>
